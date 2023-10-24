0144a69247714424ed00e072744326d0502f40b4
Anh Lam Truong <85408593+anhlamtruong@users.noreply.github.com>
2023-10-23 21:49:20 -0400

    good

---
+++ app/(dashboard)/_componets/sidebar-routes.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Layout, Compass } from "lucide-react";
+import SidebarItem from "./sidebar-item";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/dashboard",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+];
+
+const SidebarRoutes = () => {
+  const routes = guestRoutes;
+
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SidebarRoutes;
--- app/(dashboard)/_componets/sidebar.tsx
+++ app/(dashboard)/_componets/sidebar.tsx
@@ -1,11 +1,14 @@
 import Logo from "./logo";
+import SidebarRoutes from "./sidebar-routes";
 const Sidebar = () => {
   return (
     <div className="h-full border-r flex flex-col overflow-auto bg-white shadow-sm">
       <div className="p-6">
         <Logo></Logo>
       </div>
-      <div className="flex flex-col w-full"></div>
+      <div className="flex flex-col w-full">
+        <SidebarRoutes></SidebarRoutes>
+      </div>
     </div>
   );
 };
