76f1b381c41f652d0db8b5c151ab56e76361ab4d
Minh Tran <ngocminh7102000@gmail.com>
2023-10-31 16:55:05 -0400

    test

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,10 +3,11 @@ import Sidebar from "./_componets/sidebar";
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className=" h-full">
+      <div className="h-[80px] md:pl-56"></div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      {children}
+      <main className="md:pl-56 h-full">{children}</main>
     </div>
   );
 };
