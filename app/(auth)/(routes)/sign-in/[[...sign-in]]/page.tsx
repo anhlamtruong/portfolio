085b92c17270ed8fb2f8988b32cbbdff375f1c06
Anh Lam Truong <85408593+anhlamtruong@users.noreply.github.com>
2023-11-05 23:27:19 -0500

    test

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "@/app/(auth)/_components/auth_form";
 
 export default function Page() {
   return (
-    <div className="md:w-1/2 lg:w-2/3 sm:w-1/3">
+    <div className="md:w-1/2 lg:w-2/3 sm:w-1/3 justify-center items-center">
       <AuthForm></AuthForm>
     </div>
   );
