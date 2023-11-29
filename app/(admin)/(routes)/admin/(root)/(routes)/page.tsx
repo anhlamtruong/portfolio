f5aa66ff8fda41b1e9bbb1f9241d782b6b939207
Anh Lam Truong <85408593+anhlamtruong@users.noreply.github.com>
2023-11-29 00:18:50 -0500

    test

--- app/(admin)/(routes)/admin/(root)/(routes)/page.tsx
+++ app/(admin)/(routes)/admin/(root)/(routes)/page.tsx
@@ -1,20 +1,35 @@
 "use client";
 
 import UserButton from "@/app/(auth)/_components/auth_account_button";
+import LoadingCarrot from "@/components/ui/loading/loading-carrot";
+import useAsyncDataFetcher from "@/hooks/useAsyncDataFetcher";
 import { useStoreModal } from "@/hooks/useStoreModal";
 import { ModalProvider } from "@/providers/modal_provider";
+import { Store } from "@prisma-client-mysql";
+import { useRouter } from "next/navigation";
 
 import React, { useEffect } from "react";
 
 const AdminPage: React.FC = ({}) => {
   const onOpen = useStoreModal((state) => state.onOpen);
+  const onClose = useStoreModal((state) => state.onClose);
   const isOpen = useStoreModal((state) => state.isOpen);
+  const router = useRouter();
+
+  const { data, isLoading, error } = useAsyncDataFetcher<Store>("api/admin");
 
   useEffect(() => {
     if (!isOpen) {
       onOpen();
     }
-  }, [onOpen, isOpen]);
+    if (data) {
+      onClose();
+      router.push(`/admin/${data.id}`);
+    }
+  }, [onOpen, isOpen, data, onClose, router]);
+  if (isLoading) {
+    return <LoadingCarrot></LoadingCarrot>;
+  }
 
   return (
     <div>
--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,23 +1,4 @@
-import getCurrentUser from "@/actions/getCurrentUser";
-import getFirstStore from "@/actions/getFirstStore";
-import { ModalProvider } from "@/providers/modal_provider";
-import { redirect } from "next/navigation";
-import prismaMySQL from "@/lib/service/prisma_mysql";
-
-const AdminLayout = async ({ children }: { children: React.ReactNode }) => {
-  const user = await getCurrentUser();
-  if (!user?.id) {
-    redirect("/sign-in");
-  }
-  const store = await prismaMySQL.store.findFirst({
-    where: {
-      userId: user?.id,
-    },
-  });
-
-  if (store) {
-    redirect(`/admin/${store.id}`);
-  }
+const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return <>{children}</>;
 };
 
